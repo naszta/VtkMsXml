7d80e17c9d51c5dbfb7712d6a8aeabc1c8c13e76
Nasztanovics Ferenc <naszta@naszta.hu>
2011-09-05 16:57:25 +0200

    STL stream based class was added.

---
+++ vtkSequentialStream.cxx
@@ -0,0 +1,59 @@
+#include "vtkSequentialStream.h"
+
+vtkSequentialStream::vtkSequentialStream( std::istream &is )
+  : is(is), ref_count(0)
+{
+  InitializeCriticalSection( &this->critical_section );
+}
+
+vtkSequentialStream::~vtkSequentialStream( void )
+{
+  DeleteCriticalSection( &this->critical_section );
+}
+
+// IUnknown
+HRESULT __stdcall vtkSequentialStream::QueryInterface( const IID &riid, void ** ppvObject )
+{
+  if ( riid == IID_ISequentialStream )
+  {
+    *ppvObject = static_cast<void*>(this); 
+    this->AddRef();
+    return S_OK;
+  }
+  if (riid == IID_IUnknown)
+  {
+    *ppvObject = static_cast<void*>(this); 
+    this->AddRef();
+    return S_OK;
+  }
+  *ppvObject = 0;
+  return E_NOINTERFACE;
+}
+
+ULONG __stdcall vtkSequentialStream::AddRef( void )
+{
+  return InterlockedIncrement(&this->ref_count);
+}
+
+ULONG __stdcall vtkSequentialStream::Release( void )
+{
+  ULONG nRefCount = InterlockedDecrement(&this->ref_count);
+  if ( nRefCount == 0 ) delete this;
+  return nRefCount;
+}
+
+// ISequentialStream
+HRESULT __stdcall vtkSequentialStream::Read( void *pv, ULONG cb, ULONG *pcbRead )
+{
+  EnterCriticalSection( &this->critical_section );
+  this->is.read( reinterpret_cast<char*>(pv), cb );
+  *pcbRead = static_cast<ULONG>( this->is.gcount() );
+  LeaveCriticalSection( &this->critical_section );
+  return S_OK;
+}
+
+HRESULT __stdcall vtkSequentialStream::Write( void const *pv, ULONG cb, ULONG *pcbWritten )
+{
+  *pcbWritten = cb;
+  return S_OK;
+}
---
+++ vtkSequentialStream.h
@@ -0,0 +1,27 @@
+#ifndef vtkSequentialStreamH
+#define vtkSequentialStreamH 1
+
+#include <Objidl.h>
+#include <istream>
+
+class vtkSequentialStream : public 
+{
+public:
+  vtkSequentialStream( std::istream &is );
+  virtual ~vtkSequentialStream( void );
+
+  // IUnknown
+  virtual HRESULT __stdcall QueryInterface( const IID &riid, void ** ppvObject );
+  virtual ULONG __stdcall AddRef( void );
+  virtual ULONG __stdcall Release( void );
+
+  // ISequentialStream
+  virtual HRESULT __stdcall Read( void *pv, ULONG cb,  ULONG *pcbRead );
+  virtual HRESULT __stdcall Write( void const *pv, ULONG cb, ULONG *pcbWritten );
+  
+protected:
+  std::istream &is;
+  CRITICAL_SECTION critical_section;
+  ULONG ref_count;
+};
+#endif
